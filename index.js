7b295b3dc24e08eb6b20a79c8ff1572d63453906
Rahat73 <rahat.ashik.18@gmail.com>
2023-09-28 13:22:52 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oahoku5.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const menusCollection = client.db("DinerDynasty").collection("Menus");
+    const reviewsCollection = client.db("DinerDynasty").collection("Reviews");
+
+    ////////////////////////MenusCollection////////////////////////
+    app.get("/menu", async (req, res) => {
+      const result = await menusCollection.find().toArray();
+      res.send(result);
+    });
+    ////////////////////////MenusCollection////////////////////////
+    //----------------------------------------------------------------------------//
+    ////////////////////////ReviewsCollection////////////////////////
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    });
+    ////////////////////////ReviewsCollection////////////////////////
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Diner Dynasty is running");
+});
+
+app.listen(port, () => {
+  console.log(`Diner Dynasty is running on port: ${port}`);
+});

54d90ca4440e552b309b4a9e84115921d5228f43
Rahat73 <rahat.ashik.18@gmail.com>
2023-10-07 03:49:24 +0600

    verify admin, user,menu apis added

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 5000;
 //middlewares
 app.use(cors());
 app.use(express.json());
+
 const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
   if (!authorization) {
@@ -48,6 +49,18 @@ async function run() {
     const reviewsCollection = client.db("DinerDynasty").collection("Reviews");
     const cartsCollection = client.db("DinerDynasty").collection("Carts");
 
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { userEmail: email };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== "admin") {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden access" });
+      }
+      next();
+    };
+
     ////////////////////////JWT////////////////////////
     app.post("/jwt", (req, res) => {
       const user = req.body;
@@ -61,7 +74,7 @@ async function run() {
     //----------------------------------------------------------------------------//
 
     ////////////////////////UsersCollection////////////////////////
-    app.get("/users", async (req, res) => {
+    app.get("/users", verifyJWT, verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
@@ -74,6 +87,17 @@ async function run() {
         return res.send({ message: "User already exists" });
       }
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      if (req.decoded.email !== email) {
+        return res.send({ admin: false });
+      }
+      const query = { userEmail: email };
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
       res.send(result);
     });
 
@@ -100,8 +124,21 @@ async function run() {
     //----------------------------------------------------------------------------//
 
     ////////////////////////MenusCollection////////////////////////
-    app.get("/menu", async (req, res) => {
+    app.get("/menus", async (req, res) => {
       const result = await menusCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/menus", verifyJWT, verifyAdmin, async (req, res) => {
+      const newItem = req.body;
+      const result = await menusCollection.insertOne(newItem);
+      res.send(result);
+    });
+
+    app.delete("/menus/:id", verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menusCollection.deleteOne(query);
       res.send(result);
     });
     ////////////////////////MenusCollection////////////////////////

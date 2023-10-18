a1c1776daf18ffb6143793e1277f78729cba851d
Rahat73 <rahat.ashik.18@gmail.com>
2023-10-18 15:52:09 +0600

    vercel deploy connection fix

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
     const usersCollection = client.db("DinerDynasty").collection("Users");
     const menusCollection = client.db("DinerDynasty").collection("Menus");
     const reviewsCollection = client.db("DinerDynasty").collection("Reviews");
